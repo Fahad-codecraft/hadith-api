29e7253575120c449e300244e641a7c5c3053654
Fahad Devnikar <devnikarfahad@gmail.com>
2024-06-08 15:23:26 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import app from './src/app.js'
 
 const run = async () => {
     try {
-        const port = process.env.PORT || 3000
+        const port = process.env.PORT || 6000
         app.listen(port, () => {
             console.log(`Server is up on port ${port}`);
         })
